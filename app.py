7bb7e47107958ddf0995958d535a60f132ed4938
Chandra Bhushan Singh <work.chandu21@gmail.com>
2025-08-21 01:29:05 +0530

    add menu items to menu

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ with col1:
     st.image('unnamed.jpg')
     
 with col2:
-    st.header('CampusX is an ONlinr platform')
+    st.header('CampusX is an ONline platform')
 
 st.header('Courses')
 st.subheader('DSMP')
 st.subheader('DAMP')
 st.subheader('DEMP')
 st.subheader('DSA')
+
+st.sidebar.title('Menu')
+st.sidebar.markdown("""
+- Home 
+- About
+- Contact                   
+                    """)
