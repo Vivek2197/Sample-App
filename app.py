251b1d6a2213bc49894b4a57ab5f9d100a418c59
Chandra Bhushan Singh <work.chandu21@gmail.com>
2025-08-21 00:37:37 +0530

    add image to home page

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title('CampusX')
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image('unnamed.jpg')
+    
+with col2:
+    st.header('CampusX is an ONlinr platform')
+    
